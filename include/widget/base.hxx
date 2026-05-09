c897d4ee0703841f1ff08e25ef9d2cb3d8ca404a
giorgi.page <andreagiorgino1@gmail.com>
2026-05-09 17:22:00 +0200

    base widget class templated

--- include/widget/base.hxx
+++ include/widget/base.hxx
@@ -5,52 +5,68 @@
 #include "raylib.h"
 
 namespace widget {
-    class base;
-    using base_ptr = std::shared_ptr<base>;
+    template<class Derived = void>
+        class base : std::enable_shared_from_this<Derived> {
+            public:
+                using base_ptr = std::shared_ptr<base>;
+                using const_base_ptr = std::shared_ptr<const base>;
 
-    class base {
-        public:
-            base(void) noexcept = default;
-            base(::Rectangle rec) noexcept;
+                using shared_ptr = std::shared_ptr<Derived>;
+                using const_shared_ptr = std::shared_ptr<const Derived>;
 
-            base(const base&) noexcept = default;
-            auto operator =(const base&) noexcept -> base& = default;
+            public:
+                base(void) noexcept = default;
 
-            base(base&&) noexcept = default;
-            auto operator =(base&&) noexcept -> base& = default;
+                base(::Rectangle rec) noexcept {
+                    _pos = { rec.x, rec.y };
+                    _size = { rec.width, rec.height };
+                }
 
-            virtual ~base(void) noexcept = 0;
+                base(const base&) noexcept = default;
+                auto operator =(const base&) noexcept -> base& = default;
 
-        public:
-            virtual auto check_collision(void) const noexcept -> bool;
+                base(base&&) noexcept = default;
+                auto operator =(base&&) noexcept -> base& = default;
 
-            template<class Self>
-                auto&& on_render(this Self&& self, std::function<void(const base&)> event);
-            template<class Self>
-                auto&& on_update(this Self&& self, std::function<void(base&)> event);
+                virtual ~base(void) noexcept {}
 
-            virtual auto render(void) const -> void;
-            virtual auto update(void) -> void;
+            public:
+                virtual auto check_collision(void) const noexcept -> bool {
+                    return ::CheckCollisionPointRec(::GetMousePosition(), {
+                                _pos.x,
+                                _pos.y,
+                                _size.x,
+                                _size.y,
+                            });
+                }
 
-        protected:
-            ::Vector2 _pos {};
-            ::Vector2 _size {};
+                template<class Self>
+                    auto&& on_render(this Self&& self, std::function<void(const const_shared_ptr)> event) {
+                        self._renderCallback = event;
+                        return std::forward<Self>(self);
+                    }
 
-            std::function<void(const base&)> _renderCallback {};
-            std::function<void(base&)> _updateCallback {};
-    };
+                template<class Self>
+                    auto&& on_update(this Self&& self, std::function<void(const shared_ptr)> event) {
+                        self._updateCallback = event;
+                        return std::forward<Self>(self);
+                    }
 
-    template<class Self>
-        auto&& base::on_render(this Self&& self,
-                std::function<void(const base&)> event) {
-            self._renderCallback = event;
-            return std::forward<Self>(self);
-        }
+                virtual auto render(void) const -> void {
+                    if (_renderCallback)
+                        _renderCallback(this->shared_from_this());
+                }
 
-    template<class Self>
-        auto&& base::on_update(this Self&& self,
-                std::function<void(base&)> event) {
-            self._updateCallback = event;
-            return std::forward<Self>(self);
-        }
+                virtual auto update(void) -> void {
+                    if (_updateCallback)
+                        _updateCallback(this->shared_from_this());
+                }
+
+            protected:
+                ::Vector2 _pos {};
+                ::Vector2 _size {};
+
+                std::function<void(const const_shared_ptr)> _renderCallback {};
+                std::function<void(const shared_ptr)> _updateCallback {};
+        };
 } // namespace widget
