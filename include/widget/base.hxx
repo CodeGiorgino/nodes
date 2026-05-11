926a721465ba103ce3b89cde19f3e08a05fba67a
giorgi.page <andreagiorgino1@gmail.com>
2026-05-11 13:29:41 +0200

    code cleanup and factory implemented

--- include/widget/base.hxx
+++ include/widget/base.hxx
@@ -6,7 +6,7 @@
 
 namespace widget {
     template<class Derived = void>
-        class base : std::enable_shared_from_this<Derived> {
+        class base : public std::enable_shared_from_this<Derived> {
             public:
                 using base_ptr = std::shared_ptr<base>;
                 using const_base_ptr = std::shared_ptr<const base>;
@@ -15,8 +15,6 @@ namespace widget {
                 using const_shared_ptr = std::shared_ptr<const Derived>;
 
             public:
-                base(void) noexcept = default;
-
                 base(::Rectangle rec) noexcept {
                     _pos = { rec.x, rec.y };
                     _size = { rec.width, rec.height };
@@ -31,6 +29,18 @@ namespace widget {
                 virtual ~base(void) noexcept {}
 
             public:
+                static auto create(void) noexcept -> shared_ptr {
+                    return std::make_shared<Derived>();
+                }
+
+                auto position(void) const noexcept -> ::Vector2 {
+                    return _pos;
+                }
+
+                auto size(void) const noexcept -> ::Vector2 {
+                    return _size;
+                }
+
                 virtual auto check_collision(void) const noexcept -> bool {
                     return ::CheckCollisionPointRec(::GetMousePosition(), {
                                 _pos.x,
@@ -40,27 +50,11 @@ namespace widget {
                             });
                 }
 
-                template<class Self>
-                    auto&& on_render(this Self&& self, std::function<void(const const_shared_ptr)> event) {
-                        self._renderCallback = event;
-                        return std::forward<Self>(self);
-                    }
+                virtual auto render(void) const -> void = 0;
+                virtual auto update(void) -> void = 0;
 
-                template<class Self>
-                    auto&& on_update(this Self&& self, std::function<void(const shared_ptr)> event) {
-                        self._updateCallback = event;
-                        return std::forward<Self>(self);
-                    }
-
-                virtual auto render(void) const -> void {
-                    if (_renderCallback)
-                        _renderCallback(this->shared_from_this());
-                }
-
-                virtual auto update(void) -> void {
-                    if (_updateCallback)
-                        _updateCallback(this->shared_from_this());
-                }
+            protected:
+                base(void) = default;
 
             protected:
                 ::Vector2 _pos {};
